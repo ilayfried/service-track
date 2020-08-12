4f1a0a0c6ce0ac2fb2979e847f21269607376228
ilayfried <ilay.fried@gmail.com>
2020-08-12 13:27:02 +0300

    init

---
+++ AspNetWebApiRest1/Controllers/ListItemsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using AspNetWebApiRest1.Models;
+using System.Web;
+using System.Net.Http;
+using System.Net;
+
+namespace AspNetWebApiRest1
+{
+    public class ListItemsController : ApiController
+    {
+
+        private static List<CustomListItem> _listItems { get; set; } = new List<CustomListItem>();
+        // GET api/values 
+        public IEnumerable<CustomListItem> Get()
+        {
+            return _listItems;
+        }
+
+        // GET api/values/5 
+        public HttpResponseMessage Get(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // POST api/values 
+        public HttpResponseMessage Post([FromBody]CustomListItem model)
+        {
+            if (string.IsNullOrEmpty(model?.Text))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            int maxId = 0;
+            if (_listItems.Count > 0)
+            {
+                maxId = _listItems.Max(x => x.Id);
+            }
+            model.Id = maxId + 1;
+            _listItems.Add(model);
+            return Request.CreateResponse(HttpStatusCode.Created, model);
+        }
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public HttpResponseMessage Delete(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _listItems.Remove(item);
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+    }
+    
+}
